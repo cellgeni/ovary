94a1bae8c11fa64f66713bd43b70c92950ca290b
cellgeni <cellgeni@sanger.ac.uk>
2025-06-24 23:15:16 +0100

    add blacklist to snapatac2

--- 05.snapatac2_peak_calling.sh
+++ 05.snapatac2_peak_calling.sh
@@ -4,7 +4,7 @@ nextflow ../actions/nf-atac/main.nf \
  --min_counts 3500 \
  --max_counts 10000000 \
  --celltypes ../work/scanvi_out/combined_gene_matrix_plus_ref_clean_scanvi_raw_obs.csv \
- --output_dir results_snapatac2_call_peaks_with_blacklist \
+ --output_dir results_snapatac2_call_peaks_replicates \
  --fragments_filename fragments.tsv.gz \
  --remove_doublets True \
  -resume
